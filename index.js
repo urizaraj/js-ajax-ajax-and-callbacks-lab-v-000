d41ea9c80571296f177a8250653c63759e759b55
Alexander Urizar <urizaraj@gmail.com>
2018-03-31 15:28:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function displayRepositories(data) {
 function displayError(error) {
   console.log('error', error)
   const results = $('#results')[0]
-  console.log(results)
+  console.log('results', results)
   results.html('there was an error, please try again')
 
 }
