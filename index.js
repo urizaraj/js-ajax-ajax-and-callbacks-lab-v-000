7922da12f6e27da66202a1b5c5ef24ee8ed3bbf4
Alexander Urizar <urizaraj@gmail.com>
2018-03-31 15:01:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ function displayRepositories(data) {
   console.log('success', data)
   details.html = ''
   data.items.forEach(rep => {
-    console.log(rep)
-    details.html += `
-      <div>${rep.name}<br>${rep.html_url}</div>
-    `
+    details.html += `<div>${rep.name}<br>${rep.html_url}</div>`
   })
 }
 
