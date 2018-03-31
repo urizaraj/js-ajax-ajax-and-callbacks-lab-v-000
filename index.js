65a875e2c43e92e7b4dbe57c97e36234e898af31
Alexander Urizar <urizaraj@gmail.com>
2018-03-31 14:45:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const searchTerms = $('#searchTerms')[0]
 
 function searchRepositories() {
   const url = 'https://api.github.com/search/repositories'
-  console.log(searchTerms)
-  const term = searchTerms.value.params()
+  const term = searchTerms.value().params()
 
   console.log(term)
 }
